0035de49f786e8f4303c934c727aecf4dc2aae76
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-06 10:11:07 +0200

    Brew formula update for frontastic-cli-pre version 1.7.2-pre

--- Formula/frontastic-cli-pre.rb
+++ Formula/frontastic-cli-pre.rb
@@ -5,24 +5,24 @@
 class FrontasticCliPre < Formula
   desc ""
   homepage "https://frontastic.cloud"
-  version "1.7.1-pre4"
+  version "1.7.2-pre"
   bottle :unneeded
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/FrontasticGmbH/frontastic-cli-pre/releases/download/1.7.1-pre4/frontastic-cli-pre_1.7.1-pre4_darwin_amd64.tar.gz"
-      sha256 "69ed0ca4eafe2897ad517560725b1b403ae037d4df08b8eff8c8bac50672980c"
-    end
     if Hardware::CPU.arm?
-      url "https://github.com/FrontasticGmbH/frontastic-cli-pre/releases/download/1.7.1-pre4/frontastic-cli-pre_1.7.1-pre4_darwin_arm64.tar.gz"
-      sha256 "cc4a737d947a0b5ce8af54dafd6b85acc9fe28ea6f578a38895e82b984b8f5f8"
+      url "https://github.com/FrontasticGmbH/frontastic-cli-pre/releases/download/1.7.2-pre/frontastic-cli-pre_1.7.2-pre_darwin_arm64.tar.gz"
+      sha256 "01e5890612a7d00954137a74b8873cd73b4d330b72c745ee5530764c45ce48ce"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/FrontasticGmbH/frontastic-cli-pre/releases/download/1.7.2-pre/frontastic-cli-pre_1.7.2-pre_darwin_amd64.tar.gz"
+      sha256 "d6b1c03371fca99557148adb9288fc9ee57ec6cd27679e45318b13f4b6ebebc4"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/FrontasticGmbH/frontastic-cli-pre/releases/download/1.7.1-pre4/frontastic-cli-pre_1.7.1-pre4_linux_amd64.tar.gz"
-      sha256 "4f21eaf76c199053f85d8815cb7c4f730be0baa2b55dbfdebaf9f60644b3b9b6"
+      url "https://github.com/FrontasticGmbH/frontastic-cli-pre/releases/download/1.7.2-pre/frontastic-cli-pre_1.7.2-pre_linux_amd64.tar.gz"
+      sha256 "c0f3ed3a47c9940a886bf73963dd57fe79d2dfddafa51d46919192b00101c8d7"
     end
   end
 
