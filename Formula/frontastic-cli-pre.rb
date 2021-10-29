dd73306e2ea232d8015f6af942577814a77aace6
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-29 15:23:28 +0200

    Brew formula update for frontastic-cli-pre version 1.8.1-pre

--- Formula/frontastic-cli-pre.rb
+++ Formula/frontastic-cli-pre.rb
@@ -5,32 +5,43 @@
 class FrontasticCliPre < Formula
   desc ""
   homepage "https://frontastic.cloud"
-  version "1.8.0-pre"
-  bottle :unneeded
+  version "1.8.1-pre"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/FrontasticGmbH/frontastic-cli-pre/releases/download/1.8.0-pre/frontastic-cli-pre_1.8.0-pre_darwin_amd64.tar.gz"
-      sha256 "a2387416164ff082519a242145a767bc7b92e6c45932d2f6a6a1ddad1ee6dba5"
-    end
     if Hardware::CPU.arm?
-      url "https://github.com/FrontasticGmbH/frontastic-cli-pre/releases/download/1.8.0-pre/frontastic-cli-pre_1.8.0-pre_darwin_arm64.tar.gz"
-      sha256 "ced4abf449a8540e3744b30844facd1f5a994513c857e335c0285c995c618d89"
+      url "https://github.com/FrontasticGmbH/frontastic-cli-pre/releases/download/1.8.1-pre/frontastic-cli-pre_1.8.1-pre_darwin_arm64.tar.gz"
+      sha256 "65a1021e3bb05bf673c3bc471a5f1d048e40cf6205dde1036c949e3ed42145dc"
+
+      def install
+        bin.install "frontastic"
+        prefix.install Dir["*"]
+        ohai "\n\nRun 'frontastic init' to get started.\n"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/FrontasticGmbH/frontastic-cli-pre/releases/download/1.8.1-pre/frontastic-cli-pre_1.8.1-pre_darwin_amd64.tar.gz"
+      sha256 "3cc0d493eb4de8df68ef1cf330a99cfd6230084e8cd2e9f43fbd4f5aa6a669af"
+
+      def install
+        bin.install "frontastic"
+        prefix.install Dir["*"]
+        ohai "\n\nRun 'frontastic init' to get started.\n"
+      end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/FrontasticGmbH/frontastic-cli-pre/releases/download/1.8.0-pre/frontastic-cli-pre_1.8.0-pre_linux_amd64.tar.gz"
-      sha256 "732756945f8d426ba4d66a79c0d3571201e287c451ee6b926e20fd1acafd75f3"
+      url "https://github.com/FrontasticGmbH/frontastic-cli-pre/releases/download/1.8.1-pre/frontastic-cli-pre_1.8.1-pre_linux_amd64.tar.gz"
+      sha256 "836d57fedc56862cd0cc9e634b6a214a8a66f3970ed1de993530f5087cf4c2b1"
+
+      def install
+        bin.install "frontastic"
+        prefix.install Dir["*"]
+        ohai "\n\nRun 'frontastic init' to get started.\n"
+      end
     end
   end
 
   conflicts_with "frontastic-cli"
-
-  def install
-    bin.install "frontastic"
-    prefix.install Dir["*"]
-    ohai "\n\nRun 'frontastic init' to get started.\n"
-  end
 end
