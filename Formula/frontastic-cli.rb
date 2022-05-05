e5275ecb571722726c39725b5a9c25c85308a3a8
goreleaserbot <goreleaser@carlosbecker.com>
2022-05-05 15:49:38 +0200

    Brew formula update for frontastic-cli version 1.18.0

--- Formula/frontastic-cli.rb
+++ Formula/frontastic-cli.rb
@@ -5,12 +5,12 @@
 class FrontasticCli < Formula
   desc ""
   homepage "https://frontastic.cloud"
-  version "1.17.3"
+  version "1.18.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/FrontasticGmbH/frontastic-cli/releases/download/1.17.3/frontastic-cli_1.17.3_darwin_arm64.tar.gz"
-      sha256 "f7d0e4cd45cf6b9b8b6783cc62b124efee03616c16a9facf9734ef04440b02ce"
+      url "https://github.com/FrontasticGmbH/frontastic-cli/releases/download/1.18.0/frontastic-cli_1.18.0_darwin_arm64.tar.gz"
+      sha256 "c021fe47acbaf94890685783395f0a5792eaf8fe0226c6925a73fa827f080208"
 
       def install
         bin.install "frontastic"
@@ -19,8 +19,8 @@ class FrontasticCli < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/FrontasticGmbH/frontastic-cli/releases/download/1.17.3/frontastic-cli_1.17.3_darwin_amd64.tar.gz"
-      sha256 "eb43dbff3ee3e22add098e19910299166b48b1e039acc9f6b7133fd639a4156e"
+      url "https://github.com/FrontasticGmbH/frontastic-cli/releases/download/1.18.0/frontastic-cli_1.18.0_darwin_amd64.tar.gz"
+      sha256 "781affd2b810cb5948f41683b4531ee5697fea3e9f652723d02d94c35a143249"
 
       def install
         bin.install "frontastic"
@@ -32,8 +32,8 @@ class FrontasticCli < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/FrontasticGmbH/frontastic-cli/releases/download/1.17.3/frontastic-cli_1.17.3_linux_amd64.tar.gz"
-      sha256 "f9dc5a9e62c68802083404c70838e7937398535fd21646541a0dfa45fe0f05ea"
+      url "https://github.com/FrontasticGmbH/frontastic-cli/releases/download/1.18.0/frontastic-cli_1.18.0_linux_amd64.tar.gz"
+      sha256 "467ddaa0968c5eb77a355e7976838f71d082873c65996ddb7247732e61ea35df"
 
       def install
         bin.install "frontastic"
